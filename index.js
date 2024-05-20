bea503b732ebd310b2ca4820ee9a451c92e6c3bf
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2024-05-20 09:38:20 +0400

    back

--- index.js
+++ index.js
@@ -34,7 +34,99 @@ const productSchema = new Schema({
 
 const Product = mongoose.model("Product", productSchema);
 
+//! Add new product
+app.post("/products", async (req, res) => {
+  try {
+    const { imgSrc, title, price } = req.body;
+    const product = new Product({
+      imgSrc,
+      title,
+      price,
+    });
+    await product.save();
 
+    res.status(201).send(product);
+  } catch (err) {
+    res.status(500).json({
+      message: err,
+    });
+  }
+});
+
+//! Get products
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+
+    res.status(200).send(products);
+  } catch (err) {
+    res.status(500).json({
+      message: err,
+    });
+  }
+});
+
+//! Delete product
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    res.status(200).send(product);
+  } catch (err) {
+    res.status(500).json({
+      message: err,
+    });
+  }
+});
+
+//! Get product by Id
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).send(product);
+  } catch (err) {
+    res.status(500).json({
+      message: err,
+    });
+  }
+});
+
+//! Update product
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { imgSrc, title, price } = req.body;
+
+    const findedProduct = await Product.findById(id);
+
+    if (!findedProduct) {
+      res.status(404).json({ message: "Product not found" });
+    }
+
+    const product = await Product.findByIdAndUpdate(
+      id,
+      {
+        imgSrc,
+        title,
+        price,
+      },
+      { new: true }
+    );
+
+    res.status(200).send(product);
+  } catch (err) {
+    res.status(500).json({
+      message: err,
+    });
+  }
+});
 
 const PORT = process.env.PORT;
 const DB = process.env.DB_URL;
